2eee425f29d63f1b0a25ed4e08de927e1fc4d821
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2020-04-17 22:21:01 +0200

    Refactoring names in file_impex

--- file_impex.py
+++ file_impex.py
@@ -8,25 +8,25 @@ EXPORTS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'export
 CSV_HEADER = 'Datum,Wert,Buchungswährung,Typ,Notiz\n'
 
 
-def save_loan_transactions_to_csv(loan_interests, folder=EXPORTS_FOLDER, filename=TIMESTAMP + '_auxmoney.csv'):
-    sys.stdout.write('===== saving loan interests to CSV\r\n')
+def save_loan_transactions_to_csv(loan_transactions, folder=EXPORTS_FOLDER, filename=TIMESTAMP + '_auxmoney.csv'):
+    sys.stdout.write('===== saving loan transactions to CSV\r\n')
     sys.stdout.write('      folder: {}\r\n'.format(folder))
     sys.stdout.write('      filename: {}\r\n'.format(filename))
-    sys.stdout.write('      number of entries: {}\r\n'.format(len(loan_interests)))
+    sys.stdout.write('      number of entries: {}\r\n'.format(len(loan_transactions)))
     sys.stdout.flush()
     if not os.path.exists(folder):
         os.makedirs(folder)
     with open(os.path.join(folder, filename), 'w+', encoding='UTF-8') as output_file:
         output_file.write(CSV_HEADER)
-        for loan_interest_transaction in loan_interests:
-            output_file.write(convert_loan_to_csv(loan_interest_transaction))
+        for transaction in loan_transactions:
+            output_file.write(convert_transaction_to_csv_row(transaction))
 
 
-def convert_loan_to_csv(transaction):
-    movie_csv = '' + transaction['date'] + ',' + \
-                '"' + str(transaction['value']).replace('.', ',') + '",' + \
-                'EUR,' + \
-                '' + transaction['type'] + ',' + \
-                '' + transaction['id'] + ',' + \
-                '\n'
-    return movie_csv
+def convert_transaction_to_csv_row(transaction):
+    transaction_csv_row = '' + transaction['date'] + ',' + \
+                          '"' + str(transaction['value']).replace('.', ',') + '",' + \
+                          'EUR,' + \
+                          '' + transaction['type'] + ',' + \
+                          '' + transaction['id'] + ',' + \
+                          '\n'
+    return transaction_csv_row
