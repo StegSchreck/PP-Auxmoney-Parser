b779401617bb4caab431a35e3bfb3b55786ac657
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2025-04-11 18:25:29 +0200

    adapt to new selenium version

--- browser_handler.py
+++ browser_handler.py
@@ -2,8 +2,8 @@ import datetime
 import time
 
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver import Firefox, DesiredCapabilities, FirefoxProfile
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Firefox, FirefoxOptions
+from selenium.webdriver.firefox.service import Service
 from xvfbwrapper import Xvfb
 
 TIMESTAMP = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
@@ -17,18 +17,13 @@ class BrowserHandler:
             self.display.start()
 
         log_level = self._define_log_level(self.args)
-        capabilities = self._create_browser_capabilities(log_level)
-        options = self._create_browser_options(log_level)
-        profile = self._create_browser_profile(self.args.destination)
+        options = self._create_browser_options(log_level=log_level, exports_folder=self.args.destination)
+        service = Service(log_path=f"{TIMESTAMP}_geckodriver.log")
 
-        self.browser = Firefox(
-            firefox_profile=profile,
-            capabilities=capabilities,
-            options=options,
-            log_path="{timestamp}_geckodriver.log".format(timestamp=TIMESTAMP)
-        )
-        # http://stackoverflow.com/questions/42754877/cant-upload-file-using-selenium-with-python-post-post-session-b90ee4c1-ef51-4  # pylint: disable=line-too-long
+        self.browser = Firefox(options=options, service=service)
+        # https://stackoverflow.com/questions/42754877/cant-upload-file-using-selenium-with-python-post-post-session-b90ee4c1-ef51-4  # noqa: E501
         self.browser._is_remote = False  # pylint: disable=protected-access
+        self.browser.maximize_window()
 
     @staticmethod
     def _define_log_level(args):
@@ -44,35 +39,21 @@ class BrowserHandler:
         return log_level
 
     @staticmethod
-    def _create_browser_capabilities(log_level):
-        capabilities = DesiredCapabilities.FIREFOX.copy()
-        capabilities["moz:firefoxOptions"] = {
-            "log": {
-                "level": log_level,
-            },
-        }
-        return capabilities
+    def _create_browser_options(log_level: str, exports_folder: str) -> FirefoxOptions:
+        options = FirefoxOptions()
 
-    @staticmethod
-    def _create_browser_options(log_level):
-        options = Options()
         options.log.level = log_level
-        return options
-
-    @staticmethod
-    def _create_browser_profile(exports_folder):
-        profile = FirefoxProfile()
-        profile.set_preference("browser.download.folderList", 2)
-        profile.set_preference("browser.download.manager.showWhenStarting", False)
-        profile.set_preference("browser.download.dir", exports_folder)
-        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv, application/zip")
-        profile.set_preference("browser.helperApps.alwaysAsk.force", False)
-        profile.set_preference("devtools.jsonview.enabled", False)
-        profile.set_preference("media.volume_scale", "0.0")
+        options.set_preference("browser.download.folderList", 2)
+        options.set_preference("browser.download.manager.showWhenStarting", False)
+        options.set_preference("browser.download.dir", exports_folder)
+        options.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv, application/zip")
+        options.set_preference("browser.helperApps.alwaysAsk.force", False)
+        options.set_preference("devtools.jsonview.enabled", False)
+        options.set_preference("media.volume_scale", "0.0")
         # https://github.com/mozilla/geckodriver/issues/858#issuecomment-322512336
-        profile.set_preference("dom.file.createInChild", True)
+        options.set_preference("dom.file.createInChild", True)
 
-        return profile
+        return options
 
     def kill(self):
         self.browser.stop_client()
